c607ec30544c593bb6e12c38c7910d7fbdbb40fb
Maksim Dema <maksimdema@gmail.com>
2020-12-16 18:34:32 +0200

    добавляет решение 1-го задания

---
+++ js/task-1.js
@@ -0,0 +1,32 @@
+// ВАРИАНТ №1
+const itemsRef = [...document.querySelectorAll('li.item')];
+console.log(`в списке ${itemsRef.length} категории`);
+
+itemsRef
+  .map(item => {
+    const title = item.querySelector('h2');
+    const list = item.querySelectorAll('ul li');
+    return {
+      title: title.textContent,
+      list: list.length,
+    };
+  })
+  .forEach(entry => {
+    console.log(
+      `Категория: ${entry.title}, \nКоличество элементов: ${entry.list}`,
+    );
+  });
+
+//   ВАРИАНТ №2
+/*
+const itemsRef = document.querySelectorAll('li.item');
+console.log(`в списке ${itemsRef.length} категории`);
+
+itemsRef.forEach(item => {
+  const head = item.querySelector('h2');
+  console.log('Категория:', head.textContent);
+
+  const list = item.querySelectorAll('ul li');
+  console.log('Количество элементов:', list.length);
+});
+*/
