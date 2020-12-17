40a9ccc935f6916e2ec89f9fdc3a9bad149b2005
Maksim Dema <maksimdema@gmail.com>
2020-12-17 17:31:32 +0200

    добавляет решение 5-го задания

---
+++ js/task-5.js
@@ -0,0 +1,8 @@
+const inputRef = document.querySelector('#name-input');
+console.log(inputRef);
+const outputRef = document.querySelector('#name-output');
+console.log(outputRef);
+
+inputRef.addEventListener('input', event => {
+  outputRef.textContent = event.target.value;
+});
