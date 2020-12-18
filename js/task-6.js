c1a2a9220bdc51b4f544c1d1074953c3c95f9cc9
Maksim Dema <maksimdema@gmail.com>
2020-12-18 15:42:17 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,17 +3,18 @@ const attrRef = document.querySelector('[data-length]');
 
 const attrLength = Number(attrRef.dataset.length);
 
-inputRef.addEventListener('focus', () => {
+inputRef.addEventListener('focus', handleInputFocus);
+inputRef.addEventListener('blur', handleInputBlur);
+
+function handleInputFocus() {
   inputRef.classList.remove('invalid');
   inputRef.classList.remove('valid');
-});
-
-inputRef.addEventListener('blur', () => {
+}
+function handleInputBlur() {
   const length = (inputRef.textContent = event.target.value.length);
-
   if ((length > 0 && length < attrLength) || length > attrLength) {
     inputRef.classList.add('invalid');
   } else if (length === attrLength) {
     inputRef.classList.add('valid');
   }
-});
+}
